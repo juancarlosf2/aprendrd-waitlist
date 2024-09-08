23b21624a6e63ec24696e8dc0b1d6f7156284316
juancarlosf2 <jceldominicano@gmail.com>
2024-09-08 12:39:23 -0400

    change logo name

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -54,7 +54,7 @@ export const Navbar = () => {
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">APRENDRD</p>
           </NextLink>
         </NavbarBrand>
       </NavbarContent>
